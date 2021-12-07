cb1b12bbb1aa91cbdafe21322a3f36e9a063b605
Nicholas Lopes <30295636+Luterium@users.noreply.github.com>
2021-12-07 09:03:16 -0300

    Changed baseURL so that it's possible to test future changes

--- .user.js
+++ .user.js
@@ -16,7 +16,7 @@
 var script = document.createElement('script');
 script.id = 'AutoTrimps-Zek';
 //This can be edited to point to your own Github Repository URL.
-script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://raw.githubusercontent.com/Luterium/AutoTrimps/gh-pages/AutoTrimps2.js';
 //script.setAttribute('crossorigin',"use-credentials");
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
