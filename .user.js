338a5a484553c0968e580c6b0d3ee9d326f859b1
Nicholas Lopes <30295636+Luterium@users.noreply.github.com>
2021-12-07 09:22:42 -0300

    Tried adding the needed buttons to the project

--- .user.js
+++ .user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AutoTrimps-Zek
 // @version      1.0-Zek
-// @namespace    https://Zorn192.github.io/AutoTrimps
-// @updateURL    https://Zorn192.github.io/AutoTrimps/.user.js
+// @namespace    https://Luterium.github.io/AutoTrimps
+// @updateURL    https://Luterium.github.io/AutoTrimps/.user.js
 // @description  Automate all the trimps!
 // @author       zininzinin, spindrjr, Ishkaru, genBTC, Zeker0
 // @include      *trimps.github.io*
 // @include      *kongregate.com/games/GreenSatellite/trimps
-// @connect      *Zorn192.github.io/AutoTrimps*
+// @connect      *Luterium.github.io/AutoTrimps*
 // @connect      *trimps.github.io*
 // @connect      self
 // @grant        GM_xmlhttpRequest 
