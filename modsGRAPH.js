310ea2d2365da6f0a403e107ef9569375f2bb72e
Zeker0 <zorn@hotmail.co.uk>
2022-03-27 10:42:30 +0100

    Graphs Only

---
+++ modsGRAPH.js
@@ -0,0 +1,5 @@
+var script = document.createElement('script');
+script.id = 'AutoTrimps-Graphs';
+script.src = 'https://Zorn192.github.io/AutoTrimps/GraphsOnly.js';
+script.setAttribute('crossorigin',"anonymous");
+document.head.appendChild(script);
