eb8d82158205d4bc9cf2621574a680ecc9d523f1
Zeker0 <zorn@hotmail.co.uk>
2022-05-07 10:19:26 +0100

    Update SettingsGUI.js

--- SettingsGUI.js
+++ SettingsGUI.js
@@ -573,7 +573,7 @@ function initializeAllSettings() {
     //Timefarm
     document.getElementById('Rfrozencastle').parentNode.insertAdjacentHTML('afterend', '<br>');
     createSetting('Rtimefarm', 'Time Farm', 'Turn this on if you want to use Time Farming. ', 'boolean', false, null, 'Maps');
-    createSetting('Rtimefarmmaz', 'Time Farm Settings', 'Click to open the Time Farm settings. ', 'infoclick', 'false', null, 'maps');
+    createSetting('Rtimefarmmaz', 'Time Farm Settings', 'Click to open the Time Farm settings. ', 'infoclick', false, null, 'Maps');
     document.getElementById('Rtimefarmmaz').setAttribute('onclick', 'MAZLookalike("Time Farm", "Rtimefarm")');
     createSetting('Rtimefarmzone', 'TF: Zone', 'Farms for specified minutes in TF: Time at zone according to this settings value. Can use 59,61,62. ', 'multiValue', [-1], null, 'Maps');
     createSetting('Rtimefarmcell', 'TF: Cell', 'Time Farm at this Cell. -1 to run them at the default value, which is 1. ', 'multiValue', [-1], null, 'Μaps');
@@ -581,7 +581,7 @@ function initializeAllSettings() {
     createSetting('Rtimemaplevel', 'TF: Map Level', 'What map level to use. Can use -1,1,2. -1 to use a level down from world (Map Reducer mastery gives loot equal to world one level down), 0 to use world, 1 etc to use +maps. Using 0 by itself will use global level for all maps. ', 'multiValue', [0], null, 'Maps');
     createSetting('Rtimemapselection', 'TF: Map Selection', 'Select which map you prefer to use.', 'textValue', 'undefined', null, 'Μaps');
     createSetting('Rtimespecialselection', 'TF: Special Selection', 'Select which Special to use. May bug out if you cannot afford selected. Also overrides your autojobs to buy workers relating to the resource you want to farm. I.e if LFC is chosen all workers will be hired as farmers and rest fired for the duration of farm. <br> 0 = None<br>fa = Fast Attacks<br>lc = Large Cache<br>ssc = Small Savory Cache<br>swc = Small Wooden Cache<br>smc = Small Metal Cache<br>src = Small Research Cache<br>p = Prestigous<br>hc = Huge Cache<br>lsc = Large Savory Cache<br>lwc = Large Wooden Cache<br>lmc = Large Metal Cache<br>lrc = Large Research Cache ', 'textValue', 'undefined', null, 'Μaps');
-    createSetting('Rtimegatherselection', 'TF: Gather Selection', 'Select which resource to Gather. ','textValue', 'undefined', null, 'Μaps');
+    createSetting('Rtimegatherselection', 'TF: Gather Selection', 'Select which resource to Gather. ', 'textValue', 'undefined', null, 'Μaps');
 
     //Tributefarm
     document.getElementById('Rtimegatherselection').parentNode.insertAdjacentHTML('afterend', '<br>');
