fa3281daa477bef3f7d7ab459b0f733dd2f101f2
Zeker0 <zorn@hotmail.co.uk>
2022-05-18 16:36:23 +0100

    Update mods.js

--- mods.js
+++ mods.js
@@ -3,3 +3,5 @@ script.id = 'AutoTrimps-Zek';
 script.src = 'https://Zorn192.github.io/AutoTrimps/AutoTrimps2.js';
 script.setAttribute('crossorigin',"anonymous");
 document.head.appendChild(script);
+
+var isSteam = true;
