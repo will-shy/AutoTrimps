544ddff0fd4bf82d643571376266a3e456412c35
Zeker0 <zorn@hotmail.co.uk>
2022-08-01 14:07:37 +0100

    Update GraphsOnly.js

--- GraphsOnly.js
+++ GraphsOnly.js
@@ -296,7 +296,7 @@ function pushData() {
         currentTime: new Date().getTime(),
         portalTime: game.global.portalTime,
         world: game.global.world,
-        challenge:  === 'Daily' ? game.global.challengeActive + " " + getDailyTimeString().toString().replace(/(\d{4})(\d{2})(\d+)/, '$1-$2-$3') : game.global.challengeActive,
+        challenge: game.global.challengeActive == 'Daily' ? game.global.challengeActive + " " + getDailyTimeString().toString().replace(/(\d{4})(\d{2})(\d+)/, '$1-$2-$3') : game.global.challengeActive,
         voids: game.global.totalVoidMaps,
         heirlooms: { value: game.stats.totalHeirlooms.value, valueTotal: game.stats.totalHeirlooms.valueTotal },
         nullifium: recycleAllExtraHeirlooms(true),
