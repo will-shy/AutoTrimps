34ceaa9ab7c872e4f01ee47bb5f02ed721b8f791
Zeker0 <zorn@hotmail.co.uk>
2022-08-02 19:10:13 +0100

    Update SettingsGUI.js

--- SettingsGUI.js
+++ SettingsGUI.js
@@ -876,7 +876,6 @@ function initializeAllSettings() {
     createSetting('Rmanageequality', 'Manage Equality', 'Manages Equality for you. Sets Equality to 0 on Slow enemies, and Autoscaling on for Fast enemies. ', 'boolean', 'false', null, 'Combat');
     createSetting('Rcalcfrenzy', 'Frenzy Calc', '<b>Experimental. </b><br>Adds frenzy to the calc. Be warned\, it will not farm as much with this on as it expects 100% frenzy uptime. ', 'boolean', 'false', null, 'Combat');
     createSetting('Rmutecalc', 'Mute Calc', 'What zone to start calculating Mutations at. 0 to disable.', 'value', '-1', null, 'Combat');
-    createSetting('Rmutecalcattack', 'Mute Attack', 'What kind of mutation attack to calculate for your H:D. The higher the multiplier you select here the more it will farm. Recommend half nova, as that will also calculate rage pretty well. Full nova can be used on certain runs if you need to farm a lot more. ', 'dropdown', 'Off', ["Off", "x5 Half Nova", "x10 Full Nova"], 'Combat');
 
 
     //Scryer
@@ -1979,7 +1978,6 @@ function updateCustomButtons() {
     radonon ? turnOn("Rmanageequality") : turnOff("Rmanageequality");
     radonon ? turnOn("Rcalcfrenzy") : turnOff("Rcalcfrenzy");
     radonon ? turnOn("Rmutecalc") : turnOff("Rmutecalc");
-    radonon &&  getPageSetting('Rmutecalc') > 0 ? turnOn("Rmutecalcattack") : turnOff("Rmutecalcattack");
 
 
 
@@ -2310,7 +2308,6 @@ function updateCustomButtons() {
     document.getElementById('RdHeliumHourChallenge').value = autoTrimpSettings.RdHeliumHourChallenge.selected;
     document.getElementById('mapselection').value = autoTrimpSettings.mapselection.selected;
     document.getElementById('Rmapselection').value = autoTrimpSettings.Rmapselection.selected;
-    document.getElementById('Rmutecalcattack').value = autoTrimpSettings.Rmutecalcattack.selected;
     document.getElementById('Prestige').value = autoTrimpSettings.Prestige.selected;
     document.getElementById('AutoGoldenUpgrades').value = autoTrimpSettings.AutoGoldenUpgrades.selected;
     document.getElementById('dAutoGoldenUpgrades').value = autoTrimpSettings.dAutoGoldenUpgrades.selected;
