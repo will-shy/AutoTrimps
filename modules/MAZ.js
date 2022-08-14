52c6183a890f9aefb32726cbf80aafa1b54ef76b
Zeker0 <zorn@hotmail.co.uk>
2022-08-14 12:25:49 +0100

    Update MAZ.js

--- modules/MAZ.js
+++ modules/MAZ.js
@@ -37,6 +37,10 @@ function MAZLookalike(titleText, isItIn, event) {
         map = 'Rtributemapselection';
         special = 'Rtributespecialselection';
         gather = 'Rtributegatherselection';
+    } else if (titleText.includes('Shrine')) {
+        zone = 'Rshrinezone';
+        cell = 'Rshrinecell';
+        setting = 'Rshrineamount';
     } else if (titleText.includes('Quagmire')) {
         zone = 'Rblackbogzone';
         setting = 'Rblackbogamount';
@@ -110,6 +114,8 @@ function MAZLookalike(titleText, isItIn, event) {
         tooltipText += "<div class='windowLevel'>Level</div>"
         tooltipText += "<div class='windowSpecial'>Special</div>"
         tooltipText += "<div class='windowGather'>Gather</div>"
+    } else if (titleText.includes('Shrine')) {
+        tooltipText += "<div class='windowSetting'>Amount</div>"
     } else if (titleText.includes('Quagmire')) {
         tooltipText += "<div class='windowSetting'>Black Bogs</div>"
     } else if (titleText.includes('Insanity')) {
@@ -170,6 +176,8 @@ function MAZLookalike(titleText, isItIn, event) {
                 vals.level = autoTrimpSettings[level].value[x] ? autoTrimpSettings[level].value[x] : 0;
                 vals.special = autoTrimpSettings[special].value[x] ? autoTrimpSettings[special].value[x] : 0;
                 vals.gather = autoTrimpSettings[gather].value[x] ? autoTrimpSettings[gather].value[x] : 0;
+            } else if (titleText.includes('Shrine')) {
+                vals.setting = autoTrimpSettings[setting].value[x] ? autoTrimpSettings[setting].value[x] : 0;
             } else if (titleText.includes('Quagmire')) {
                 vals.setting = autoTrimpSettings[setting].value[x] ? autoTrimpSettings[setting].value[x] : 0;
             } else if (titleText.includes('Insanity')) {
@@ -219,6 +227,8 @@ function MAZLookalike(titleText, isItIn, event) {
             tooltipText += "<div class='windowLevel'><input value='" + vals.level + "' type='number' id='windowLevel" + x + "'/></div>";
             tooltipText += "<div class='windowSpecial' onchange='updateWindowPreset(" + x + ")'><select value='" + vals.special + "' id='windowSpecial" + x + "'>" + specialsDropdown + "</select></div>"
             tooltipText += "<div class='windowGather' onchange='updateWindowPreset(" + x + ")'><select value='" + vals.gather + "' id='windowGather" + x + "'>" + gatherDropdown + "</select></div>"
+        } else if (titleText.includes('Shrine')) {
+            tooltipText += "<div class='windowSetting'><input value='" + vals.setting + "' type='number' id='windowSetting" + x + "'/></div>";
         } else if (titleText.includes('Quagmire')) {
             tooltipText += "<div class='windowSetting'><input value='" + vals.setting + "' type='number' id='windowSetting" + x + "'/></div>";
         } else if (titleText.includes('Insanity')) {
@@ -310,6 +320,10 @@ function settingsWindowSave(titleText, reopen) {
             map = 'Rtributemapselection';
             special = 'Rtributespecialselection';
             gather = 'Rtributegatherselection';
+        } else if (titleText.includes('Shrine')) {
+            zone = 'Rshrinezone';
+            cell = 'Rshrinecell';
+            setting = 'Rshrineamount';
         } else if (titleText.includes('Quagmire')) {
             zone = 'Rblackbogzone';
             setting = 'Rblackbogamount';
@@ -372,6 +386,8 @@ function settingsWindowSave(titleText, reopen) {
             map = document.getElementById('windowMap' + x).value;
             special = document.getElementById('windowSpecial' + x).value;
             gather = document.getElementById('windowGather' + x).value;
+        } else if (titleText.includes('Shrine')) {
+            setting = document.getElementById('windowSetting' + x).value;
         } else if (titleText.includes('Quagmire')) {
             setting = document.getElementById('windowSetting' + x).value;
         } else if (titleText.includes('Insanity')) {
@@ -459,6 +475,8 @@ function settingsWindowSave(titleText, reopen) {
         autoTrimpSettings[setting].value = [];
         autoTrimpSettings[special].value = [];
         autoTrimpSettings[gather].value = [];
+    } else if (titleText.includes('Shrine')) {
+        autoTrimpSettings[setting].value = [];
     } else if (titleText.includes('Quagmire')) {
         autoTrimpSettings[setting].value = [];
     } else if (titleText.includes('Insanity')) {
@@ -501,6 +519,8 @@ function settingsWindowSave(titleText, reopen) {
             autoTrimpSettings[setting].value[x] = thisSetting[x].setting
             autoTrimpSettings[special].value[x] = thisSetting[x].special
             autoTrimpSettings[gather].value[x] = thisSetting[x].gather
+        } else if (titleText.includes('Shrine')) {
+            autoTrimpSettings[setting].value[x] = thisSetting[x].setting
         } else if (titleText.includes('Quagmire')) {
             autoTrimpSettings[setting].value[x] = thisSetting[x].setting
         } else if (titleText.includes('Insanity')) {
