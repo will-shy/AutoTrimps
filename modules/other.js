5255cd234779ba687916f65210b952731ee70676
Zeker0 <zorn@hotmail.co.uk>
2022-08-15 13:35:33 +0100

    Update other.js

--- modules/other.js
+++ modules/other.js
@@ -4111,15 +4111,10 @@ function hypofrag() {
 function autoshrinecharge(amount) {
     var zone;
     if (amount != null) {
-    console.log("amount was not null");
         if (amount != 0) {
-            console.log("amount was not 0 and here is amount: " + amount);
             game.permaBoneBonuses.boosts.consume();
-            console.log("CONSUMED");
             autoTrimpSettings.Rshrinecharge.value++;
-            console.log("charge up in value and heres value now: " + autoTrimpSettings.Rshrinecharge.value);
             zone = autoTrimpSettings.zonetracker;
-            console.log("zone: " + zone);
         }
     }
     if (zone != game.global.world) {
@@ -4133,25 +4128,15 @@ function autoshrine() {
         var shrinezone = getPageSetting('Rshrinezone');
         console.log("shrinezone is: " + shrinezone);
         if (shrinezone.includes(game.global.world)) {
-            var shrineamount = [0];
+            var shrineamount = getPageSetting('Rshrineamount');
             var shrineindex = shrinezone.indexOf(game.global.world);
             var shrinecell = getPageSetting('Rshrinecell')[shrineindex];
-
-            shrinezone = getPageSetting('Rshrinezone');
-            shrineamount = getPageSetting('Rshrineamount');
-
-            var shrineindex = shrinezone.indexOf(game.global.world);
             var shrinezones = shrineamount[shrineindex];
-            
-            console.log("shrinezones is: " + shrinezones);
 
             shrinezones = shrinezones - autoTrimpSettings.Rshrinecharge.value;
-            
-            console.log("shrinezones is after charge setting: " + shrinezones);
 
             if (game.global.lastClearedCell + 2 >= shrinecell && shrinezones > 0) {
                 autoshrinecharge(shrinezones);
-                console.log("executing autoshrinecharge");
             }
         }
     }
